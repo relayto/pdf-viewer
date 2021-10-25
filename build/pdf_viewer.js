aa2feb7fe74e866406988306dfd615b9d6d784a7
ievgen <siemenspromaster@gmail.com>
2021-10-25 18:36:12 +0300

    Set canvas wrapper only when renders like svg

--- build/pdf_viewer.js
+++ build/pdf_viewer.js
@@ -7373,8 +7373,10 @@
                 this.renderingState =
                   _pdf_rendering_queue.RenderingStates.RUNNING;
                 var canvasWrapper = document.createElement("div");
-                canvasWrapper.style.width = div.style.width;
-                canvasWrapper.style.height = div.style.height;
+                if(this.renderer === _ui_utils.RendererType.SVG) {
+                  canvasWrapper.style.width = div.style.width;
+                  canvasWrapper.style.height = div.style.height;
+                }
                 canvasWrapper.classList.add("canvasWrapper");
 
                 if (this.annotationLayer && this.annotationLayer.div) {
