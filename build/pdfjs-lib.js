686555e1d53cae21729cb4c49d69bf32f665e75a
siemenspromaster <siemenspromaster@gmail.com>
2021-11-15 18:48:53 +0200

    Enable IE11 support webpack target:es5

--- build/pdfjs-lib.js
+++ build/pdfjs-lib.js
@@ -7,22 +7,22 @@
 		var a = typeof exports === 'object' ? factory(require("pdfjsLib"), require("pdfjsViewer")) : factory(root["pdfjsLib"], root["pdfjsViewer"]);
 		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
 	}
-})(self, function(__WEBPACK_EXTERNAL_MODULE__23__, __WEBPACK_EXTERNAL_MODULE__626__) {
-return /******/ (() => { // webpackBootstrap
+})(self, function(__WEBPACK_EXTERNAL_MODULE__544__, __WEBPACK_EXTERNAL_MODULE__231__) {
+return /******/ (function() { // webpackBootstrap
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
-/***/ 23:
-/***/ ((module) => {
+/***/ 544:
+/***/ (function(module) {
 
-module.exports = __WEBPACK_EXTERNAL_MODULE__23__;
+module.exports = __WEBPACK_EXTERNAL_MODULE__544__;
 
 /***/ }),
 
-/***/ 626:
-/***/ ((module) => {
+/***/ 231:
+/***/ (function(module) {
 
-module.exports = __WEBPACK_EXTERNAL_MODULE__626__;
+module.exports = __WEBPACK_EXTERNAL_MODULE__231__;
 
 /***/ })
 
@@ -54,30 +54,28 @@ module.exports = __WEBPACK_EXTERNAL_MODULE__626__;
 /******/ 	
 /************************************************************************/
 /******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
+/******/ 	!function() {
 /******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = (exports) => {
+/******/ 		__webpack_require__.r = function(exports) {
 /******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
 /******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
 /******/ 			}
 /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
 /******/ 		};
-/******/ 	})();
+/******/ 	}();
 /******/ 	
 /************************************************************************/
 var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
-(() => {
+!function() {
 // ESM COMPAT FLAG
 __webpack_require__.r(__webpack_exports__);
 
 // EXTERNAL MODULE: external "pdfjsLib"
-var external_pdfjsLib_ = __webpack_require__(23);
+var external_pdfjsLib_ = __webpack_require__(544);
 // EXTERNAL MODULE: external "pdfjsViewer"
-var external_pdfjsViewer_ = __webpack_require__(626);
+var external_pdfjsViewer_ = __webpack_require__(231);
 ;// CONCATENATED MODULE: ./src/lib/pdfjs-lib-facade.js
-var _excluded = ["onPassword", "onProgress"];
-
 function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
 
 function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
@@ -119,7 +117,7 @@ var PDFJsFacade = function PDFJsFacade() {
   _defineProperty(this, "createDocLoadingTask", function (params) {
     var onPassword = params.onPassword,
         onProgress = params.onProgress,
-        getParams = _objectWithoutProperties(params, _excluded);
+        getParams = _objectWithoutProperties(params, ["onPassword", "onProgress"]);
 
     var url = getParams.url;
     return new Promise(function (resolve, reject) {
@@ -148,8 +146,7 @@ window.rtPDFJsLib = new PDFJsFacade();
 window.pdfjsLib = external_pdfjsLib_;
 ;// CONCATENATED MODULE: ./src/pdfjs-lib.js
 
-})();
-
+}();
 /******/ 	return __webpack_exports__;
 /******/ })()
 ;
